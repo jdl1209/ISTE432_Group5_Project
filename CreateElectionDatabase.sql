e8cfdf30fc6ee8ab559e16650145564bae470f71
jdl1209 <54865107+jdl1209@users.noreply.github.com>
2022-10-28 12:41:27 -0400

    Update CreateElectionDatabase.sql

--- CreateElectionDatabase.sql
+++ CreateElectionDatabase.sql
@@ -6,14 +6,14 @@ USE election;
 DROP TABLE IF EXISTS election_info;
 CREATE TABLE election_info (
 
-	  SocietyName VARCHAR(200) NOT NULL,
-    SocietyContact VARCHAR(50),
-    ElectionName VARCHAR(200) NOT NULL,
-    ElectionStartTime DATE,
-    ElectionEndTime DATE,
-    SocietyLogo VARCHAR(21844),
-    SocietyColor VARCHAR(20),
-    SocietyPhoneNumber VARCHAR(12)
+    'society_name' VARCHAR(200) NOT NULL,
+    'society_contact' VARCHAR(50),
+    'election_name' VARCHAR(200) NOT NULL,
+    'election_start_time' DATE,
+    'election_end_time' DATE,
+    'society_logo' VARCHAR(21844),
+    'society_color' VARCHAR(20),
+    'society_phone_number' VARCHAR(12)
 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -45,3 +45,47 @@ CREATE  TABLE IF NOT EXISTS `candidate` (
   `candidate_picture` VARCHAR(50) ,
   PRIMARY KEY (`candidate_id`) )
 ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS voter;
+
+CREATE TABLE voter (
+    member_id INT NOT NULL,
+    voter_id INT NOT NULL,
+    fname VARCHAR(35) NOT NULL,
+    lname VARCHAR(35) NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (member_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE user (
+    user_id INT NOT NULL,
+    user_type_id INT NOT NULL,
+    email VARCHAR(40),
+    username VARCHAR(30),
+    password VARCHAR(30),
+    PRIMARY KEY (user_id)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8; 
+
+CREATE TABLE voter_status (
+    member_id INT NOT NULL,
+    status SMALLINT NOT NULL, 
+    FOREIGN KEY (member_id) REFERENCES voter(member_id)
+)   ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE status (
+    yesNo VARCHAR(5) NOT NULL,
+    status SMALLINT NOT NULL,
+    PRIMARY KEY (yesNo)
+    FOREIGN KEY (status) REFERENCES voter_status(status)    
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE admin (
+    admin_id INT NOT NULL,
+    user_id INT NOT NULL,
+    username VARCHAR(30),
+    password VARCHAR(30),
+    PRIMARY KEY (admin_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id) 
+
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
