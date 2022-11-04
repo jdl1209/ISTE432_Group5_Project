c91669c4eed776f60ea42662cc413f89a2cd3bdf
jdl1209 <54865107+jdl1209@users.noreply.github.com>
2022-11-04 13:25:02 -0400

    Update login.php
    Added php at top of the file

--- login.php
+++ login.php
@@ -1,3 +1,19 @@
+<?php
+   include("config.php");
+   include("functions.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      checkLogin($myusername, $mypassword);
+      
+   }
+?>
+<!DOCTYPE html>
 <html>
    
    <head>
