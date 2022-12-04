a5ff8dbb04edc4c3533cf3989314136d6e68c974
jdl1209 <jdl1209@rit.edu>
2022-12-04 18:44:56 -0500

    added admin login

--- functions.php
+++ functions.php
@@ -17,4 +17,21 @@
 
    }
 
+   function checkAdminLogin($username, $password){
+
+      $sql = "SELECT admin_id FROM admin WHERE username=$username AND password=$password";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+
+         return 1;
+
+      } else {
+
+         return 0;
+
+      }
+
+   }
+
 ?>
