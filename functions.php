4cd260d3e16ed9bc360e05298300594b4dfdef7e
Chris P <92759606+cpegoli@users.noreply.github.com>
2022-12-04 21:54:57 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -56,5 +56,37 @@
 
    }
 
+// Check if the user is logged in
+if (isset($_SESSION['user_id'])) {
+  // Get the user's ID from the session
+  $user_id = $_SESSION['user_id'];
+
+  // Connect to the database
+  $conn = new mysqli('localhost', 'database_username', 'database_password', 'database_name');
+
+  // Check if the user has voted in the election
+  $query = "SELECT * FROM votes WHERE user_id = '$user_id' AND election_id = '$election_id'";
+  $result = $conn->query($query);
+
+  if ($result->num_rows == 1) {
+    // The user has voted, get the voting method
+    $vote = $result->fetch_assoc();
+    $voting_method = $vote['ballot_type'];
+
+    if ($voting_method == 'paper') {
+      // The user voted using a paper ballot
+        return 0;
+    } else if ($voting_method == 'online') {
+      // The user voted using an online ballot
+      return 1;
+    }
+  } else {
+    // The user has not voted
+    return -1;
+  }
+}
+
+?>
+
 
 ?>
